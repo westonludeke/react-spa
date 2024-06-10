9c6c6440245715283d9d93d5f059bfbd48b2b30f
westonludeke <westonludeke@gmail.com>
2024-06-10 17:25:16 -0500

    reupload project

---
+++ src/Page2.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>Page2ページです</h1>
+      <Link to="/page2/999">UrlParameter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query Parameter</Link>
+    </div>
+  );
+};
