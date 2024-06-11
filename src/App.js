a9a78da54a8969c8153203a41a8d5e5de6853196
westonludeke <westonludeke@gmail.com>
2024-06-10 19:01:01 -0500

    update App.js to add commandbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Router } from './router/Router';
+import { init } from "commandbar";
 import './styles.css';
 
-export default function App() {
+if (typeof window !== "undefined") {
+  init("1f4d41c8");
+}
+
+export default () => {
   useEffect(() => {
-    const loggedInUserId = '12345'; // example user ID
-    if (window.CommandBar) {
-      window.CommandBar.boot(loggedInUserId);
-    } else {
-      console.error('CommandBar SDK is not loaded');
-    }
+		// Here we are passing in an empty string as the userId and no userAttributes param. 
+		// In a real application you would want to include these so you can tag
+		// analytics events and for end-user personalization.
+    window.CommandBar.boot("");
+
+    return () => {
+      window.CommandBar.shutdown();
+    };
   }, []);
 
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Link to="/">HOME</Link>
-        <br />
-        <Link to="/Page1">Page1</Link>
-        <br />
-        <Link to="/Page2">Page2</Link>
-      </div>
-      <Router />
-    </BrowserRouter>
-  );
-}
+  return null;
+};
+
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         <Link to="/">HOME</Link>
+//         <br />
+//         <Link to="/Page1">Page1</Link>
+//         <br />
+//         <Link to="/Page2">Page2</Link>
+//       </div>
+//       <Router />
+//     </BrowserRouter>
+//   );
+// }
