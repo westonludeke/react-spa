26c0eacc49c03b080dd2508ae5d4bdd13372dba9
westonludeke <westonludeke@gmail.com>
2024-06-13 18:43:16 -0500

    update index.html and App.js for Get Cat Facts action

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { Router } from './router/Router';
 import { init } from 'commandbar';
 import './styles.css';
 
-if (typeof window !== 'undefined') {
-  init('1f4d41c8');
-}
+const fetchCatFacts = async () => {
+  try {
+    console.log('Fetching cat facts...');
+    const response = await fetch('https://meowfacts.herokuapp.com/');
+    console.log('Response:', response);
+    const data = await response.json();
+    console.log('Data:', data);
+    return data.data[0];
+  } catch (error) {
+    console.error('Error fetching cat facts:', error);
+  }
+};
 
 const App = () => {
-  useEffect(() => {
-    window.CommandBar.boot('');
+  const [fact, setFact] = useState('');
 
-    return () => {
-      window.CommandBar.shutdown();
-    };
-  }, []);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      console.log('Initializing CommandBar');
+      init('1f4d41c8');
+      console.log('CommandBar instance:', CommandBar);
+  
+      if (typeof CommandBar !== 'undefined') {
+        console.log('Booting CommandBar');
+        CommandBar.boot('');
+  
+        const setupCommandBarAction = async () => {
+          console.log('Setting up CommandBar action');
+          CommandBar.addCallback('Get Cat Fact', async () => {
+            console.log('CommandBar action triggered');
+            const catFact = await fetchCatFacts();
+            console.log('Cat Fact:', catFact);
+            setFact(catFact);
+            console.log('State updated with new fact');
+          });
+          console.log('Callback added');
+        };
+  
+        setupCommandBarAction();
+  
+        return () => {
+          console.log('Shutting down CommandBar');
+          CommandBar.shutdown();
+        };
+      } else {
+        console.error('CommandBar is not available.');
+      }
+    }
+  }, []);  
 
   return (
     <BrowserRouter>
@@ -25,6 +62,7 @@ const App = () => {
         <Link to="/page1">Page1</Link>
         <br />
         <Link to="/page2">Page2</Link>
+        {fact && <p>{fact}</p>}
       </div>
       <Router />
     </BrowserRouter>
